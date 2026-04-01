9f8cbf0910e75d6880c456278193e95e54910a79
Zhifan Ye <littleye233@gmail.com>
2026-04-02 02:55:53 +0800

    🧑‍💻 chore: add NixOS development environment configuration

---
+++ shell.nix
@@ -0,0 +1,29 @@
+let
+
+  pkgs = import <nixpkgs> {};
+  mypkgs = builtins.getFlake "git+https://codeberg.org/LittleYe233/packages.nix.git";
+  libcron = mypkgs.packages.${builtins.currentSystem}.libcron;
+  quickjs = mypkgs.packages.${builtins.currentSystem}.quickjspp;
+
+in
+
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    gcc
+    gdb
+    cmake # should add in this scope
+    pkg-config
+    clang-tools
+    python313Packages.ninja
+  ];
+
+  buildInputs = [
+    pkgs.curlFull
+    pkgs.rapidjson
+    pkgs.toml11
+    pkgs.yaml-cpp
+    pkgs.pcre2
+    quickjs
+    libcron
+  ];
+}
