dac78f4e603a89acfaf81e17e9138f4148ab13bf
Zhifan Ye <littleye233@gmail.com>
2026-04-02 02:54:10 +0800

    🚨 fix: properly set LibCron include dirs as CMake list

--- cmake/FindLibCron.cmake
+++ cmake/FindLibCron.cmake
@@ -4,7 +4,7 @@ find_path(DATE_INCLUDE_DIR date/date.h)
 find_library(LIBCRON_LIBRARY libcron)
 
 set(LIBCRON_LIBRARIES "${LIBCRON_LIBRARY}")
-set(LIBCRON_INCLUDE_DIRS "${LIBCRON_INCLUDE_DIR} ${DATE_INCLUDE_DIR}")
+set(LIBCRON_INCLUDE_DIRS ${LIBCRON_INCLUDE_DIR} ${DATE_INCLUDE_DIR})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(LibCron DEFAULT_MSG
